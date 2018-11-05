bd326555855cb6ee6c37031ae7104f3a54efc949
zza00000 <cailx@getui.com>
2018-11-05 10:15:22 +0800

    add python class

--- Python/基础知识/class.py
+++ Python/基础知识/class.py
@@ -1,12 +1,10 @@
 class Modu:
     def __init__(self):
-        self.data=data
+        self.data=[]
 
-    def sum(a=90, b=80):
+    def sum(self, a=90, b=80):
         print(a-b);
         return;
 
 mod = Modu()
-mod.sum
-# class ModuSon(Modu):
-
+mod.sum()
